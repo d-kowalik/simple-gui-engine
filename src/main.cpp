8ae95a8cb5f2513252cc855e217108516df925e9
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-01-03 01:40:11 +0100

    Draw a rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -66,9 +66,39 @@ int main() {
     delete[] info_log;
   }
 
+  float vertices[] = {
+      -.5f, -.5f, 0.f, 1.f, .5f, 1.f,
+      -.5f, .5f, 0.f, .5f, 1.f, 1.f,
+      .5f, .5f, 0.f, 1.f, .5f, .5f,
+      .5f, -.5f, 0.f, 1.f, 1.f, .5f
+  };
+
+  unsigned indices[] = {
+      0, 1, 3,
+      1, 2, 3
+  };
+
+  unsigned vao, vbo, ebo;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), nullptr);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  glGenBuffers(1, &ebo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   while (!window.ShouldClose()) {
     window.Clear();
+
+    glUseProgram(shader_program);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
     window.Update();
   }
