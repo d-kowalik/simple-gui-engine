a623d397ecc1f2150e4f2e35d6f695037404b60e
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-01-13 04:03:21 +0100

    Remove unnecessary code from main

--- src/main.cpp
+++ src/main.cpp
@@ -34,35 +34,6 @@ int main() {
   const auto text_fragment_shader = Graphics::Shader(GL_FRAGMENT_SHADER, "../src/Shaders/text.frag");
   const auto text_shader_program = Graphics::ShaderProgram({text_vertex_shader, text_fragment_shader});
 
-  float vertices[] = {
-      .0f, .0f, .5f, 1.f, .5f, 1.f,
-      .0f, 50.f, .5f, .5f, 1.f, 1.f,
-      50.f, 50.f, .5f, 1.f, .5f, .5f,
-      50.f, .0f, .5f, 1.f, 1.f, .5f
-  };
-
-  unsigned indices[] = {
-      0, 1, 3,
-      1, 2, 3
-  };
-
-  unsigned vao, vbo, ebo;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), nullptr);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-
-  glGenBuffers(1, &ebo);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-
   FT_Library ft;
   if (FT_Init_FreeType(&ft)) printf("Could not init FreeType\n");
   Graphics::Font arial{"../fonts/arial.ttf", 48, ft};
@@ -92,19 +63,11 @@ int main() {
     HandleInput(dt);
 
     view = camera.View();
-    model = glm::rotate(glm::mat4(1.0f), glm::radians((float)glfwGetTime()) * dt * 500.0f, {.0f, .0f, 1.f});
+    model = glm::mat4(1.0f);
 
     shader_program.Use();
     shader_program.SetUniformMat4f("view", view);
     shader_program.SetUniformMat4f("model", model);
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
-    model = glm::mat4(1.0f);
-    shader_program.SetUniformMat4f("model", model);
-
     text_shader_program.Use();
     text_shader_program.SetUniformMat4f("view", view);
     font_renderer.Render("ddddTest tekstu yzxvbniJQ", {.0f, .0f}, 1.0f,
