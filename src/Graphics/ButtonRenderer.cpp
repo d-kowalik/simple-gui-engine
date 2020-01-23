3f87be0249f08db82348921b623f255ee1c09f59
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-01-23 13:22:11 +0100

    Improve readability in ButtonRenderer

--- src/Graphics/ButtonRenderer.cpp
+++ src/Graphics/ButtonRenderer.cpp
@@ -9,12 +9,11 @@ sge::Graphics::ButtonRenderer::ButtonRenderer(sge::Graphics::FontRenderer *font_
 void sge::Graphics::ButtonRenderer::DrawButton(const std::string &text, glm::vec2 position, glm::vec2 size,
                                                glm::vec3 fill_color, glm::vec3 text_color) {
   auto height = _font_renderer->GetFont().GetSize();
-  const float text_scale = std::min(size.x / (height * text.size()), size.y / height);
+  const float text_scale = std::min(size.x / (float)(height * text.size()), size.y / height);
   auto final_size = _font_renderer->CheckSize(text, text_scale);
   _rectangle_renderer->Draw(size, position, fill_color);
-  _font_renderer->Render(text,
-                         {position.x + size.x / 2 - final_size.x / 2, position.y + size.y / 2 - final_size.y / 2},
-                         text_scale,
-                         text_color);
+
+  glm::vec2 text_position{position.x + size.x / 2 - final_size.x / 2, position.y + size.y / 2 - final_size.y / 2};
+  _font_renderer->Render(text, text_position, text_scale, text_color);
 }
 
--- src/Graphics/ButtonRenderer.hpp
+++ src/Graphics/ButtonRenderer.hpp
@@ -5,14 +5,17 @@
 
 namespace sge::Graphics {
   class ButtonRenderer {
+    constexpr static glm::vec3 DEFAULT_FILL_COLOR = {0.f, 0.f, 0.f};
+    constexpr static glm::vec3 DEFAULT_TEXT_COLOR = {1.f, 1.f, 1.f};
+
     FontRenderer *_font_renderer;
     RectangleRenderer *_rectangle_renderer;
 
   public:
     ButtonRenderer(FontRenderer *font_renderer, RectangleRenderer *rectangle_renderer);
 
-    void DrawButton(const std::string &text, glm::vec2 position, glm::vec2 size, glm::vec3 fill_color = {0.f, 0.f, 0.f},
-                    glm::vec3 text_color = {1.f, 1.f, 1.f});
+    void DrawButton(const std::string &text, glm::vec2 position, glm::vec2 size, glm::vec3 fill_color = DEFAULT_FILL_COLOR,
+                    glm::vec3 text_color = DEFAULT_TEXT_COLOR);
   };
 
 }
