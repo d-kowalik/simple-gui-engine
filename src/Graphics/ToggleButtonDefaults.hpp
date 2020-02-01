d2d56a1e5d1f7846c4be5fb9e1b94d3e737f17a0
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-02-01 01:36:12 +0100

    Add ToggleButton

---
+++ src/Graphics/ToggleButtonDefaults.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace sge::Graphics::ToggleButtonDefaults {
+  constexpr glm::vec3 TOGGLE_COLOR = {.3f, .3f, .3f};
+  constexpr bool TOGGLED = false;
+}
